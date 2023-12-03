7f0a84122368d2af39302c8b295b71c5e29cc8e6
aadheil <adilkkinbox@gmail.com>
2023-12-03 18:01:36 +0530

    ok

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,2 +1,4 @@
-export const BASEURL = 'http://localhost:4000'
+// export const BASEURL = 'http://localhost:4000'
+export const BASEURL = 'https://xgservernode.onrender.com'
+
 // 'https://xgservernode.onrender.com';
